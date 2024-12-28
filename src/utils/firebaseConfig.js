ba4891354a2d868731eff2090959ba8a5f94702d
aman <aman@101edu.co>
2024-12-29 03:23:57 +0530

    Add CRUD functionality

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzsgabRWecRk0G318ARl5_iPviv24C2kk",
+  authDomain: "hyper-local-app-test.firebaseapp.com",
+  projectId: "hyper-local-app-test",
+  storageBucket: "hyper-local-app-test.firebasestorage.app",
+  messagingSenderId: "906090428565",
+  appId: "1:906090428565:web:2da5ae97fa3ad40ff2039c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
